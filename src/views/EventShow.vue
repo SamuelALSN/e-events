f641cd7d0ee43ffc981be1bf629aa5c8325818f6
SamuelALSN <samuelgwfirst@gmail.com>
2020-07-06 23:45:43 +0000

    reorganising our code implementing an EventService

---
+++ src/views/EventShow.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Showing event : {{ event.title }}</h1>
+</template>
+
+<script>
+import EventService from '@/services/EventService'
+
+export default {
+  name: 'EventShow',
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log('There is a error :', error.response)
+      })
+  }
+}
+</script>
+
+<style scoped></style>
