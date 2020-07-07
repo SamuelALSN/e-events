a6ef23a0489430c57812134d7a22105f8e9f4e24
SamuelALSN <samuelgwfirst@gmail.com>
2020-07-07 22:24:25 +0000

    pagination: add function preventNextDisplay when there is no data to display

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,49 +1,53 @@
 <template>
-  <div>
-    <h1>Events Listing</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div>
+        <h1>Events Listing</h1>
+        <EventCard v-for="event in events" :key="event.id" :event="event"/>
 
-    <template v-if="page !== 1">
-      <router-link
-        :to="{ name: 'event-list', query: { page: page - 1 } }"
-        rel="prev"
-      >
-        Prev Page
-      </router-link>
-    </template>
+        <template v-if="page !== 1">
+            <router-link
+                    :to="{ name: 'event-list', query: { page: page - 1 } }"
+                    rel="prev"
+            >
+                Prev Page
+            </router-link>
+        </template>
 
-    ||
-     <template v-if="eventsTotal > page * 3 ">
-       <router-link :to="{ name: 'event-list', query: { page: page + 1 } }">
-         Next Page
-       </router-link>
-     </template>
+        ||
+        <template v-if=" eventDisplay ">
+            <router-link :to="{ name: 'event-list', query: { page: page + 1 } }">
+                Next Page
+            </router-link>
+        </template>
 
-  </div>
+    </div>
 </template>
 
 <script>
-import EventCard from '@/components/EventCard'
-import { mapState } from 'vuex'
-export default {
-  name: 'EventList',
-  components: {
-    EventCard
-  },
-  created() {
-    this.$store.dispatch('fetchEvents', {
-      perPage: 3,
-      page: this.page
-    })
-  },
+  import EventCard from '@/components/EventCard'
+  import { mapState } from 'vuex'
 
-  computed: {
-    page() {
-      return parseInt(this.$route.query.page) || 1
+  export default {
+    name: 'EventList',
+    components: {
+      EventCard
     },
-    ...mapState(['events', 'eventsTotal'])
+    created() {
+      this.$store.dispatch('fetchEvents', {
+        perPage: 3,
+        page: this.page
+      })
+    },
+
+    computed: {
+      page() {
+        return parseInt(this.$route.query.page) || 1
+      },
+      eventDisplay () {
+         return  this.eventsTotal > this.page * 3
+      },
+      ...mapState(['events', 'eventsTotal'])
+    }
   }
-}
 </script>
 
 <style scoped></style>
