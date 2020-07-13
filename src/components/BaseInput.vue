4d7199d2c89105e9c3b10f988ce1fd20e07849a4
SamuelALSN <samuelgwfirst@gmail.com>
2020-07-13 20:37:20 +0000

    complete form field validation with vuelidate

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="field">
+  <div>
     <label v-if="label"> {{ label }} </label>
-    <input type="text" :value="value" @input="updateValue" v-bind="$attrs" />
+    <input type="text" :value="value" @input="updateValue" v-bind="$attrs" v-on="listeners" />
   </div>
 </template>
 
@@ -19,6 +19,14 @@ export default {
   methods: {
     updateValue(event) {
       this.$emit('input', event.target.value)
+    }
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: this.updateValue
+      }
     }
   }
 }
